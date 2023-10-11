7fe17b666f235125868fff8a21f388404adccdca
szymanel100 <laskowicz.szymon@gmail.com>
2023-10-11 18:51:54 +0200

    test commit

---
+++ main.cbl
@@ -0,0 +1,14 @@
+       program-id. main.
+
+       environment division.
+       configuration section.
+
+       data division.
+       working-storage section.
+       linkage section.
+
+       procedure division.
+           
+           goback.
+
+       end program main.
